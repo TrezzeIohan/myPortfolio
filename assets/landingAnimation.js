c70dad8e089eb9c92ed7694c162198d492c5b3ec
Iohan R. Trézze <110934864+TrezzeIohan@users.noreply.github.com>
2023-01-19 11:31:49 -0300

    Delete assets directory

--- assets/landingAnimation.js
+++
@@ -1,7 +0,0 @@
-//USed for makng typing animation, but i probabl won't use
-var typed = new Typed('#typed', {
-    stringsElement: '#typed-strings',
-    typeSpeed:75,
-    backSpeed:100,
-    loop: true
-  });
